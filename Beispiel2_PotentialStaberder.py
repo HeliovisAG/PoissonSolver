01b89ade3a6b0d55b3f8a89e4b24e76461c9378a
HeliovisAG <152088093+HeliovisAG@users.noreply.github.com>
2026-03-05 09:16:49 +0100

    commit

--- Beispiel2_PotentialStaberder.py
+++ Beispiel2_PotentialStaberder.py
@@ -3,33 +3,34 @@ import matplotlib.pyplot as plt
 from poissonSolverMatrix2D import PoissonSolverMatrix2D
 
 N = 400
-L = 20
+L = 50
 h = L/N             # örtliche Auflösung
+ds = 20           # Dicke der nichtleitenden Oberfläche in pixel
+
 ps = PoissonSolverMatrix2D(N=N, L=L)
 X = ps.X
 Y = ps.Y
 
 sigma_soil=50e-3    # Elektrische Leitfähigkeit Boden (lehmig) in S/m
 sigmaCu = 5.8e7     # Elektrische Leitfähigkeit von Cu bei 20°C in 1/Ohm m
-I = 20000           # Strom der über den Staberder eingespeist wird
+I = 2000           # Strom der über den Staberder eingespeist wird
 
 # Definition des Staberders
-xE = 10             # x-Position des Erders in m
-LE = 5             # Länge des Erders in m
-dE = 0.05              # Dicke des Erders in m
-ds = 20*h           # Dicke der nichtleitenden Oberflöche in m
+xE = 25             # x-Position des Erders in m
+LE = 18             # Länge des Erders in m
+dE = 0.03              # Dicke des Erders in m
 
 # Leitfähigkeitsmatrix
 ps.sigma = ps.sigma*sigma_soil                                
-mask = (X>=xE) & (X<=xE+dE) & (Y>=L-LE-ds) & (Y<=L-ds)
-ps.sigma[mask] = sigmaCu                       
+#mask = (X>=xE) & (X<=xE+dE) & (Y>=L-LE-ds) & (Y<=L-ds)
+ps.sigma[int(xE/h)-1,:N-ds-1] = sigmaCu                       
 
 mask_surf = Y > L-ds                      # dünne isolierende Schicht an der Oberfläche, adiabatische Randbedingung
 ps.sigma[mask_surf] = 1e-8
 
 # Quellenmatrix
 V = LE*dE**2          # Volumen des Erders in m³
-ps.Q[mask] = I/V  # A/m³
+ps.Q[int(xE/h)-1,:N-ds-1] = I/V  # A/m³
 
 # Randbedingungen
 def phi_left(x, y):  # Dirichlet links
@@ -43,10 +44,21 @@ def phi_top(x, y):  # Dirichlet oben
 bc = {'left':phi_left, 'right':phi_right, 'bottom':phi_bottom, 'top':phi_top}
 
 ps.solve(bc)            # Berechnet das Potential durch Lösen der Poissongleichung
-ps.fieldvektor()        # Berechnet die Feldstärke aus dem Potential
+phi = ps.phi
+ps.fieldvektor()        # Berechnet die Feldstärke aus dem PotentialE
+E, Ex, Ey = ps.E, ps.Ex, ps.Ey
 
 # --- Visualisierung ---
 fig, ax = plt.subplots()
-ps.drawImage(ps.phi, fig, ax, 'Elektrostatisches Potential V')
-#ps.drawContour(phi, ax)
+ps.drawImage(phi, fig, ax, 'Elektrostatisches Potential V')
+# Öberfläche
+lx = np.linspace(0, L, N)
+ly = np.ones(N)*h
+ax.plot(lx, ly)
+
+fig, ax = plt.subplots()
+phi_surf = phi[N-ds, :]
+ax.plot(phi_surf)  # Potentialverlauf an der Oberfläche
+#ax.plot(phi[:])  # Staberder
+#ps.drawContour(E, ax)
 plt.show()
