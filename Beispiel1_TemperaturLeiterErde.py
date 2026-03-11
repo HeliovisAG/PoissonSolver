416763cbf67bd18398720375a9da1ea05dea843c
HeliovisAG <152088093+HeliovisAG@users.noreply.github.com>
2026-03-11 19:08:12 +0100

    Update LeiterErde

--- Beispiel1_TemperaturLeiterErde.py
+++ Beispiel1_TemperaturLeiterErde.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from poissonSolverMatrix2D import PoissonSolverMatrix2D
 
-N = 200
-L = 5.0
+N = 400
+L = 6.0
 ps = PoissonSolverMatrix2D(N=N, L=L)
 X = ps.X
 Y = ps.Y
@@ -12,19 +12,30 @@ Y = ps.Y
 ps.sigma = ps.sigma*1                                # Wärmeleitfähigkeit Boden W/mK
 
 # Leiter
-x0 = 2
-y0 = 3.2
-r=0.2
+x0 = 3
+y0 = L-0.8
+I = 774.0
+A = 240e-6       # Querschnitt in m2
+sigma20 = 5.8e7    # El. Leitfähigkeit von Cu bei 20°C in 1/Ohm m
+alpha = 3.93e-4        # Temperaturkoeffizient von Cu
+Topmax = 90
+
+R20 = 1/sigma20/A  # Widerstand in Ohm/m bei 20°C
+R_DC = R20*(1-alpha*(Topmax-20))
+R_AC = R_DC * 1.03                         # Skin- und Proximity Effekt mit 3% angenommen
+P = R_AC*I**2       # Leistung in W/m
+
+r=np.sqrt(A/np.pi)
 RR = np.sqrt((X-x0)**2 + (Y-y0)**2)
 mask = RR < r 
 ps.sigma[mask] = 380.0                       # Wärmeleitfähigkeit Kupfer W/mK
 
-# dünne isolierende Schicht an der Oberfläche, adiabatische Randbedingung
-mask_surf = Y > L*0.99
-ps.sigma[mask_surf] = 1e-8
+ps.sigma[Y > L*0.99] = 1e-8     # dünne isolierende Schicht oben, adiabatische Randbedingung 
+#ps.sigma[X < L*0.01] = 1e-8    # dünne isolierende Schicht links, adiabatische Randbedingung 
+#ps.sigma[X > L*0.99] = 1e-8    # dünne isolierende Schicht rechts, adiabatische Randbedingung 
 
 # Quellenmatrix
-ps.Q[mask] = 100                            # Quellendichte in A/m3
+ps.Q[mask] = P/A                            # Quellendichte in A/m3
 
 # Randbedingungen
 def phi_left(x, y):  # Dirichlet links
@@ -42,5 +53,5 @@ ps.solve(bc)
 # --- Visualisierung ---
 fig, ax = plt.subplots()
 #ps.drawImage(ps.phi, fig, ax, 'Temperatur °C')
-ps.drawContour(ps.phi, ax)
+ps.drawContour(ps.phi, fig, ax, 'Temperatur °C')
 plt.show()
